0304e202135495d3ac2133c6c84de1f703c56e75
cjuni <cjuni@JRSOFTWAREDEV>
2025-11-05 01:49:39 -0500

    CategoriesController and CategoryService created.

---
+++ API.J.Movies/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using API.J.Movies.DAL.Dtos.Category;
+using API.J.Movies.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.J.Movies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategoriesAsync()
+        {
+            var categoriesDto = await _categoryService.GetCategoriesAsync();
+            return Ok(categoriesDto);
+        }
+
+    }
+}
---
+++ API.J.Movies/Services/IServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using API.J.Movies.DAL.Dtos.Category;
+using API.J.Movies.DAL.Models;
+
+namespace API.J.Movies.Services.IServices
+{
+    public interface ICategoryService
+    {
+        Task<ICollection<CategoryDto>> GetCategoriesAsync();
+        Task<CategoryDto> GetCategoryAsync(int id);
+        Task<CategoryDto> CreateCategoryAsync(CategoryCreateDto categoryDto);
+        Task<CategoryDto> UpdateCategoryAsync(int id, Category categoryDto);
+        Task<bool> DeleteCategoryAsync(int id);
+        Task<bool> CategoryExistsByIdAsync(int id);
+        Task<bool> CategoryExistsByNameAsync(string name);
+    }
+
+}
